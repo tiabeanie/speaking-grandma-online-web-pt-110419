eee9404dad996cd6aef85a7838ef51afcc2b1d8d
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-07 03:01:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
+  elsif !=phrase.uppercase
+    return "NO, NOT SINCE 1983!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
