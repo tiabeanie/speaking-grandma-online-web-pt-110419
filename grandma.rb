2165d04ff48f86d3617312c32f1442020ae9f81e
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-07 02:59:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,8 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    print "I LOVE YOU TOO PUMPKIN"
+    return "I LOVE YOU TOO PUMPKIN"
   else
-    print "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
